218a83f819690979163cddb2b159a72f8dd20fca
Sanalzio <73523745+sanalzio@users.noreply.github.com>
2024-06-01 11:26:34 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 const clock = document.getElementById("clock");
 
-clock.innerHTML = new Date().toLocaleTimeString([], { hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" });;
+clock.innerHTML = new Date().toLocaleTimeString([], { hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" });
 setInterval(() => {
-    clock.innerHTML = new Date().toLocaleTimeString([], { hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" });;
+    clock.innerHTML = new Date().toLocaleTimeString([], { hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" });
 }, 1000);
